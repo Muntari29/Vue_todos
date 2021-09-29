5b910ff8d0cb422d537a7d8ad4a2d310d5d25c25
seunghee mun <65607601+Muntari29@users.noreply.github.com>
2021-09-29 17:59:33 +0900

    CreratePJT

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+const HtmlPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  resolve: {
+    extensions: [".vue", ".js"],
+    alias: {
+      "~": path.resolve(__dirname, "src"),
+    },
+  },
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: "vue-loader",
+      },
+      {
+        test: /\.s?css$/,
+        use: ["vue-style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlPlugin({
+      template: "./src/index.html", //웹팩이 해석해야하는 html 파일이 어디있는지 명시함
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from : 'static'}
+      ]
+    })
+  ],
+};
